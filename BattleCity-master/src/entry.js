cf11a9c2fd9bde9fc91359979be6e820171b9f29
hol201 <holov201@bk.ru>
2025-05-25 00:26:29 +0300

    Update entry.js

--- BattleCity-master/src/entry.js
+++ BattleCity-master/src/entry.js
@@ -1,15 +1,3 @@
 import { startGame } from './script/start';
-document.addEventListener('DOMContentLoaded', () => {
-  // Проверка на мобильное устройство
-  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
-  if (isMobile) {
-    document.body.addEventListener('touchstart', () => {
-      const audio = document.getElementById('start');
-      audio.play().then(() => {
-        audio.pause();
-        audio.currentTime = 0;
-      });
-    }, { once: true });
-  }
 (() => startGame())();
